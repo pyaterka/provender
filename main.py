a978358502e6907c11454ec12fb5cd3edb2e647c
pyaterka <peter.apple.kapustin@gmail.com>
2026-09-04 15:07:19 +1000

    Error handling

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 
 import os
+import sys
 from openai import OpenAI
 from dotenv import load_dotenv
 from prompts import system_prompt
@@ -16,32 +17,50 @@ def main():
         {"role": "system", "content": system_prompt}
     ]
 
-    while True:
+    try:
+        while True:
 
-        user_prompt = input("You: ")
-        if user_prompt.lower() in ["exit", "quit"]:
-            break
+            user_prompt = input("You: ")
+            if user_prompt.lower() in ["clear"]:
+                messages =[
+                    {"role": "system", "content": system_prompt}
+                ]
+                print("Conversation cleared.")
+                continue
+                        
+            elif user_prompt.lower() in ["exit", "quit"]:
+                break
 
-        messages.append({"role": "user", "content": user_prompt})
-        response = client.chat.completions.create(
-            model="deepseek-v4-pro",
-            messages=messages,
-            stream=False,
-            reasoning_effort="high",
-            extra_body={"thinking": {"type": "enabled"}}
-        )
+            elif len(user_prompt.strip().lower()) == 0:
+                continue
+            else:
+                messages.append({"role": "user", "content": user_prompt})
 
-        print(response.choices[0].message.content)
+            try:
+                response = client.chat.completions.create(
+                    model="deepseek-v4-pro",
+                    messages=messages,
+                    stream=False,
+                    reasoning_effort="high",
+                    extra_body={"thinking": {"type": "enabled"}}
+                )
+            except Exception as e:
+                print(f"Error : {e}")
+                break
 
-        print(f"Prompt tokens: {response.usage.prompt_tokens}")
-        print(f"Completion tokens: {response.usage.completion_tokens}")
-        print(f"Total tokens: {response.usage.total_tokens}")
+            print(response.choices[0].message.content)
 
-        assistant_reply = response.choices[0].message.content
+            print(f"Prompt tokens: {response.usage.prompt_tokens}")
+            print(f"Completion tokens: {response.usage.completion_tokens}")
+            print(f"Total tokens: {response.usage.total_tokens}")
 
-        messages.append({"role": "assistant", "content": assistant_reply})
+            assistant_reply = response.choices[0].message.content
 
+            messages.append({"role": "assistant", "content": assistant_reply})
 
+    except KeyboardInterrupt:
+        print("\nGoodbye!")
+        sys.exit(0)
 
 if __name__ == "__main__":
     main()
